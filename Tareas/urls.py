04e32b646479a3fcc0faa138dd51db28c473886e
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-05-02 00:26:03 -0500

    podra?

--- Tareas/urls.py
+++ Tareas/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('AppTarea.urls')),
+    path('tasks/',include('AppTarea.urls')),
     path('',TemplateView.as_view(template_name="index.html"),name="index")
 ]
