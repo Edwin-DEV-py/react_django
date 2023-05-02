90196ad4acae4555df42b0079c5ee95801dcc0e3
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-05-01 23:15:27 -0500

    sera?

--- cliente/src/api/tasks.api.js
+++ cliente/src/api/tasks.api.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 
-const URL = process.env.NODE_ENV === "production" ? import.meta.env.VITE_BACKEND_URL: "http://127.0.0.1:8000/api/tasks/"
+const URL =
+    process.env.NODE_ENV === "production"
+        ? import.meta.env.VITE_BACKEND_URL
+            : "http://localhost:8000";
 
 const urlApi = axios.create({
     //baseURL: 'http://127.0.0.1:8000/api/tasks/' funciona en desarrollo
-    baseURL: URL //para producion y desarrollo
+    baseURL: `${URL}/api/tasks`, //para producion y desarrollo
 })
 
 //esto pide los datos al backend
