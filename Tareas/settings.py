35b81fbe719b8a4fbafa3968e8435cdcb9af1b0f
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-05-01 22:02:11 -0500

    nuevo2

--- Tareas/settings.py
+++ Tareas/settings.py
@@ -134,6 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #guardar en memoria cache
 
+CSRF_TRUSTED_ORIGINS = ["http://*","https://web-production-1e7a4.up.railway.app"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
