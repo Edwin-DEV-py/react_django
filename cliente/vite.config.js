0b35043ba7cd32c95b92af55f7f0cfcffc56a376
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-05-02 00:41:36 -0500

    xd

--- cliente/vite.config.js
+++ cliente/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/static/' : 'https://web-production-1e7a4.up.railway.app',
+  base: process.env.NODE_ENV === 'production' ? '/static/' : '/',
 })
